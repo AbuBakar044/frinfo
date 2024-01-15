be49746f4d4cace1d878c9f5b68a2169fc6484ac
AbuBakar044 <bhattiabubakar@gmail.com>
2024-01-15 14:13:45 +0500

    Added Hive Dependencies and created Model
    Added Hive Dependencies and created Model

---
+++ lib/model/friend_model.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/foundation.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+@HiveType(typeId: 0)
+class FriendModel {
+  @HiveField(0)
+  Uint8List? image;
+
+  @HiveField(1)
+  String? name;
+
+  @HiveField(2)
+  String? number;
+
+  @HiveField(3)
+  String? desc;
+
+
+
+  //Constructor
+  FriendModel({
+    this.image,
+    this.name,
+    this.number,
+    this.desc,
+  });
+}
