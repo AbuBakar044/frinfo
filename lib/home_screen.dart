6cf22e3ee12d7fb4f247c36f59c9f3acd5ee0801
AbuBakar044 <bhattiabubakar@gmail.com>
2024-01-29 13:44:07 +0500

    Changes With Call Option and App Icon/Name Changes
    Changes With Call Option and App Icon/Name Changes

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:frinfo/colors.dart';
 import 'package:frinfo/model/friend_model.dart';
 import 'package:frinfo/routes.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -27,74 +28,76 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: greenColor,
-        title: const Text(
-          'Home Screen',
-          style: TextStyle(
+        appBar: AppBar(
+          backgroundColor: greenColor,
+          title: const Text(
+            'Home Screen',
+            style: TextStyle(
+              color: whiteColor,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: greenColor,
+          onPressed: () {
+            goNextScreen(context, const AddFriendsScreen());
+          },
+          child: const Icon(
+            Icons.add,
             color: whiteColor,
           ),
         ),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: greenColor,
-        onPressed: () {
-          goNextScreen(context, const AddFriendsScreen());
-        },
-        child: const Icon(
-          Icons.add,
-          color: whiteColor,
-        ),
-      ),
-      body: friendList.isEmpty
-          ? const Center(
-              child: Text(
-                "You don't have any friends\npress (+) button to add one!",
-                textAlign: TextAlign.center,
-              ),
-            )
-          : ValueListenableBuilder(
-              valueListenable: friendsBox!.listenable(),
-              builder: (context, _, __) {
-                friendList = friendsBox!.values.toList().cast<FriendModel>();
-                return ListView.builder(
-                  itemCount: friendList.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ListTile(
-                        tileColor: Colors.green,
-                        leading: CircleAvatar(
-                          backgroundImage:
-                              MemoryImage(friendList[index].image!),
-                        ),
-                        title: Text(
-                          friendList[index].name!,
-                          style: const TextStyle(
-                            color: whiteColor,
-                          ),
-                        ),
-                        subtitle: Text(
-                          friendList[index].number!,
-                          style: const TextStyle(
-                            color: whiteColor,
-                          ),
-                        ),
-                        trailing: IconButton(
-                            onPressed: () {
-                              deleteFriend(index);
-                            },
-                            icon: const Icon(
-                              Icons.delete,
-                              color: whiteColor,
-                            )),
+        body: ValueListenableBuilder(
+            valueListenable: friendsBox!.listenable(),
+            builder: (context, _, __) {
+              friendList = friendsBox!.values.toList().cast<FriendModel>();
+              return friendList.isEmpty
+                  ? const Center(
+                      child: Text(
+                        "You don't have any friends\npress (+) button to add one!",
+                        textAlign: TextAlign.center,
                       ),
+                    )
+                  : ListView.builder(
+                      itemCount: friendList.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListTile(
+                            tileColor: Colors.green,
+                            onLongPress: () {
+                              callMyFriend(friendList[index].number!);
+                            },
+                            leading: CircleAvatar(
+                              backgroundImage:
+                                  MemoryImage(friendList[index].image!),
+                            ),
+                            title: Text(
+                              friendList[index].name!,
+                              style: const TextStyle(
+                                color: whiteColor,
+                              ),
+                            ),
+                            subtitle: Text(
+                              friendList[index].number!,
+                              style: const TextStyle(
+                                color: whiteColor,
+                              ),
+                            ),
+                            trailing: IconButton(
+                                onPressed: () {
+                                  deleteFriend(index);
+                                },
+                                icon: const Icon(
+                                  Icons.delete,
+                                  color: whiteColor,
+                                )),
+                          ),
+                        );
+                      },
                     );
-                  },
-                );
-              }),
-    );
+            }));
   }
 
   // //Function to get friends from Hive Database
@@ -112,5 +115,11 @@ class _HomeScreenState extends State<HomeScreen> {
     friendList.removeAt(index);
     await friendsBox!.deleteAt(index);
     setState(() {});
+  }
+
+  Future<void> callMyFriend(String number) async {
+    if (!await launchUrl(Uri.parse('tel:$number'))) {
+      throw Exception('Could not launch');
+    }
   }
 }
